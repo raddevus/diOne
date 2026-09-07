ca901cc0e7eb228831df4e0fd2d21de5068f8d5f
raddevus <1radeutsch1@gmail.com>
2026-09-07 12:37:01 -0400

    Used Resolve to test how container works

--- diOne/Program.cs
+++ diOne/Program.cs
@@ -1,6 +1,15 @@
 ﻿using Models;
+using Microsoft.Practices.Unity;
+using Edict.Common.DependencyInjection;
+using Edict.Common.DependencyInjection.Unity;
+using Interfaces;
 
+var container = DependencyInjection.GetContainer();
+container.RegisterType<IMessageWriter, SpyMessageWriter>();
 Console.WriteLine("Hello, World!");
 Console.WriteLine("It's working!");
 ConsoleMessageWriter cmw = new();
 cmw.Write("test it out");
+IMessageWriter writer = container.Resolve<IMessageWriter>();
+
+writer.Write("Hello World!");
