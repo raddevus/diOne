a5e1506d42e88d8e555a4d1bc3306f4fa4978784
raddevus <1radeutsch1@gmail.com>
2026-09-07 14:20:47 -0400

    Found the RegisterType that takes the name string -- in the Unity base.  Edict wrapper removes this (by accident? or on purpose?

--- diOne/Program.cs
+++ diOne/Program.cs
@@ -4,12 +4,14 @@ using Edict.Common.DependencyInjection;
 using Edict.Common.DependencyInjection.Unity;
 using Interfaces;
 
-var container = DependencyInjection.GetContainer();
+var container = DependencyInjection.GetUnityContainer(DependencyInjection.GetContainer());
 container.RegisterType<IMessageWriter, SpyMessageWriter>();
+container.RegisterType<IMessageWriter, ConsoleMessageWriter>("console");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("It's working!");
 ConsoleMessageWriter cmw = new();
 cmw.Write("test it out");
+IMessageWriter conwriter = container.Resolve<IMessageWriter>("console");
 IMessageWriter writer = container.Resolve<IMessageWriter>();
-
+conwriter.Write("simple writer.");
 writer.Write("Hello World!");
